03eba61072ee85a78f586801fbe9ce05810a1c02
Tirso del Álamo <tirsodelalamomartin@gmail.com>
2020-07-07 10:55:51 +0200

    Map included

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,111 @@
-let myMap
 
-window.onload = () => {
+function initMap() {
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+      mapTypeControl: false,
+      center: {lat: 40.3925087, lng: -3.700465},
+      zoom: 13,
+      styles: mapStyles.aubergine
+    });
+  
+    new AutocompleteDirectionsHandler(map);
+  }
+  
+  /**
+   * @constructor
+   */
+  function AutocompleteDirectionsHandler(map) {
+    this.map = map;
+    this.originPlaceId = null;
+    this.destinationPlaceId = null;
+    this.travelMode = 'WALKING';
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsRenderer = new google.maps.DirectionsRenderer({ polylineOptions: { strokeColor: "#4cf000" } });
+    this.directionsRenderer.setMap(map);
+  
+    var originInput = document.getElementById('origin-input');
+    var destinationInput = document.getElementById('destination-input');
+    var modeSelector = document.getElementById('mode-selector');
+  
+    var originAutocomplete = new google.maps.places.Autocomplete(originInput);
+    // Specify just the place data fields that you need.
+    originAutocomplete.setFields(['place_id']);
+  
+    var destinationAutocomplete =
+        new google.maps.places.Autocomplete(destinationInput);
+    // Specify just the place data fields that you need.
+    destinationAutocomplete.setFields(['place_id']);
+  
+    this.setupClickListener('changemode-walking', 'WALKING');
+    this.setupClickListener('changemode-transit', 'TRANSIT');
+    this.setupClickListener('changemode-driving', 'DRIVING');
+  
+    this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+    this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+  
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+        destinationInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+  }
+  
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  AutocompleteDirectionsHandler.prototype.setupClickListener = function(
+      id, mode) {
+    var radioButton = document.getElementById(id);
+    var me = this;
+  
+    radioButton.addEventListener('click', function() {
+      me.travelMode = mode;
+      me.route();
+    });
+  };
+  
+  AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(
+      autocomplete, mode) {
+    var me = this;
+    autocomplete.bindTo('bounds', this.map);
+  
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+  
+      if (!place.place_id) {
+        window.alert('Please select an option from the dropdown list.');
+        return;
+      }
+      if (mode === 'ORIG') {
+        me.originPlaceId = place.place_id;
+      } else {
+        me.destinationPlaceId = place.place_id;
+      }
+      me.route();
+    });
+  };
+  
+  AutocompleteDirectionsHandler.prototype.route = function() {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
+      return;
+    }
+    var me = this;
+  
+    this.directionsService.route(
+        {
+          origin: {'placeId': this.originPlaceId},
+          destination: {'placeId': this.destinationPlaceId},
+          travelMode: this.travelMode
+        },
+        function(response, status) {
+          if (status === 'OK') {
+            console.log(response)
+            me.directionsRenderer.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+  };
+  
+  
+  
 
-    const ironhackMAD = {
-        lat: 40.3925087,
-        lng: -3.7004703
-    };
-
-
-    myMap = new google.maps.Map(document.getElementById('myMap'), {
-        zoom: 16,
-        center: ironhackMAD
-    });    
-
-}
 
